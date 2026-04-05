8ccbf6f3424df4079fc4717a09b181ca29e1a91e
tanujpsp-hub <tanujpsp@gmail.com>
2026-04-05 20:09:55 +0530

    Update WhatsAppButton.tsx

--- src/components/WhatsAppButton.tsx
+++ src/components/WhatsAppButton.tsx
@@ -9,7 +9,7 @@ interface WhatsAppButtonProps {
 
 const WhatsAppButton: React.FC<WhatsAppButtonProps> = ({ 
   phoneNumber, 
-  message = "Hello! I'm interested in learning more about Scentwave Studio." 
+  message = "Hello! I'm interested in learning more about Scentwave Organization." 
 }) => {
   const handleClick = () => {
     const encodedMessage = encodeURIComponent(message);
